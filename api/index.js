91a971c6f69e10f6735a1d8cceac4df79a17adb4
Marlon-Parra <marlonparra765@gmail.com>
2024-07-02 11:17:53 -0500

    actualizar api serever

--- api/index.js
+++ api/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid'); // Importar v4 de uuid para generar UUIDs
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Habilitar CORS para todas las solicitudes
 app.use(cors());
-
-// Middleware para manejar JSON en las solicitudes
 app.use(express.json());
 
-const dataFilePath = path.join(__dirname, '..', 'data.json'); // Asegúrate de que la ruta sea correcta
+const dataFilePath = path.join(__dirname, '..', 'data.json');
 
 function readProducts() {
     try {
@@ -31,14 +29,14 @@ function writeProducts(products) {
     }
 }
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Bienvenido a la API de productos');
 });
 
 app.post('/api/products', (req, res) => {
     const products = readProducts();
     const product = req.body;
-    product.id = uuidv4(); // Generar un UUID único
+    product.id = uuidv4();
     products.push(product);
     writeProducts(products);
     res.status(201).send(product);
@@ -57,7 +55,10 @@ app.delete('/api/products/:id', (req, res) => {
     res.status(204).send();
 });
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
 
 
 
--- public/main.js
+++ public/main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
       imagen: imagenProducto
     };
 
-    fetch('https://alurageek-xi-three.vercel.app/api/products', {
+    fetch('/api/products', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -54,21 +54,21 @@ document.addEventListener('DOMContentLoaded', function() {
     botonEliminar.classList.add('eliminar-icono');
     botonEliminar.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#fff" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
     botonEliminar.addEventListener('click', function(event) {
-      event.preventDefault(); // Evitar el comportamiento predeterminado del botón
+      event.preventDefault();
 
       const productoAEliminar = botonEliminar.closest('.producto-ejemplo');
       mostrarConfirmacion(productoAEliminar, () => {
-        fetch(`https://alurageek-xi-three.vercel.app/api/products/${producto.id}`, {
+        fetch(`/api/products/${producto.id}`, {
           method: 'DELETE'
         })
         .then(response => {
           if (!response.ok) {
             throw new Error('Error al eliminar el producto');
           }
-          return response.json(); // Devuelve el resultado para comprobar el estado de respuesta
+          return response.json();
         })
         .then(() => {
-          productoContainer.removeChild(productoAEliminar); // Elimina el producto del DOM
+          productoContainer.removeChild(productoAEliminar);
         })
         .catch(error => console.error('Error:', error));
       });
@@ -82,9 +82,8 @@ document.addEventListener('DOMContentLoaded', function() {
     productoContainer.appendChild(nuevoProducto);
   }
 
-  // Función para cargar productos
   function cargarProductos() {
-    fetch('https://alurageek-xi-three.vercel.app/api/products')
+    fetch('/api/products')
     .then(response => {
       if (!response.ok) {
         throw new Error('Error al cargar los productos');
@@ -99,12 +98,10 @@ document.addEventListener('DOMContentLoaded', function() {
     .catch(error => console.error('Error:', error));
   }
 
-  // Función para mostrar el modal de confirmación
   function mostrarConfirmacion(productoAEliminar, callback) {
     const modalConfirmacion = document.getElementById('modal-confirmacion');
     modalConfirmacion.style.display = 'block';
 
-    // Obtener las coordenadas del producto a eliminar
     const rect = productoAEliminar.getBoundingClientRect();
     modalConfirmacion.style.top = `${rect.top + window.scrollY}px`;
     modalConfirmacion.style.left = `${rect.left + window.scrollX}px`;
@@ -115,16 +112,14 @@ document.addEventListener('DOMContentLoaded', function() {
       callback();
     });
 
-    const btnCancelaEliminar = document.getElementById('btn-cancelar-eliminar');
-    btnCancelaEliminar.addEventListener('click', function() {
+    const btnCancelarEliminar = document.getElementById('btn-cancelar-eliminar');
+    btnCancelarEliminar.addEventListener('click', function() {
       modalConfirmacion.style.display = 'none';
     });
   }
 
-  // Inicializar la carga de productos al cargar la página
   cargarProductos();
 
-  // Event listeners para estilizar inputs del formulario
   const inputs = document.querySelectorAll('.formulario-producto');
   inputs.forEach(input => {
     input.addEventListener('focus', function() {
@@ -137,7 +132,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  // Función para eliminar producto predefinido (ejemplo)
   window.eliminarProductoPredefinido = function() {
     const productoPredefinido = document.querySelector('.producto-ejemplo');
     if (productoPredefinido) {
@@ -145,3 +139,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   };
 });
+
