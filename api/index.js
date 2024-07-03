acb391ca52fb8107942ba98d73843b3bf1e3175c
Marlon-Parra <163931254+Marlon-Parra@users.noreply.github.com>
2024-07-03 15:56:35 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -55,8 +55,12 @@ app.delete('/api/products/:id', (req, res) => {
     res.status(204).send();
 });
 
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
+});
+
 });
 
 
